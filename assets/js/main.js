71e7b9c59ce8b24f3e5bd47b944cbe160d4f480a
David <david.nguyen20x@gmail.com>
2022-06-23 23:42:30 -0400

    Completed the Skills Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,20 @@ if (navClose) {
         navMenu.classList.remove('show-menu')
     })
 }
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skills = document.querySelectorAll('skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.getElementsByClassName
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
