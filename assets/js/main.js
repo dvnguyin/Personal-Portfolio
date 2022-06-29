8ebbdb51302725e5ed2446dfb86c154fd3d20385
David <david.nguyen20x@gmail.com>
2022-06-28 23:34:48 -0400

    Skills Section in Progress 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,34 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navClose.LinkforEach(n => n.addEventListener('click', linkAction))
+
+const
+
+/*==== Accordian Skills ===*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.getElementsByClassName
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
